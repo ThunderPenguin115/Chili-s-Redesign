f4a169d360abe27e4f04948c48c780af8652302f
Matthew-Bueno <matthew.bueno09@gmail.com>
2026-08-18 09:13:09 -0700

    Added items we will need for the JavaScript object of arreys will need to add the right imgs later

--- data/menu.js
+++ data/menu.js
@@ -1,60 +1,112 @@
 const menuItems = [
     {
+        name: "Waffles",
+        menu: "Breakfast",
+        price: 15.99,
+        Image: "../assets/tD.avif"
+    },
+    {
+        name: "Pancakes",
+        menu: "Breakfast",
+        price: 13.59,
+        Image: "../assets/tD.avif"
+    },
+    {
+        name: "Crepes",
+        menu: "Breakfast",
+        price: 14.99,
+        Image: "../assets/tD.avif"
+    },
+    {
+        name: "French Toast",
+        menu: 'Breakfast',
+        price: 14.59,
+        Image: "../assets/tD.avif"
+    },
+    {
+        name: "Cinnamamon Buns",
+        menu: "Breakfast",
+        price: 17.99,
+        Image: "../assets/tD.avif"
+    },
+    {
+        name: "Briper",
+        menu: "Breakfast",
+        price: 25.59,
+        Image: "../assets/tD.avif"
+    },
+    {
         name: "Honey-Chipotle Chicken Crispers Combo",
         menu: "Lunch",
-        price: 17.69
+        price: 17.69,
+        Image: "../assets/tD.avif"
     },
     {
         name: "The Big QP Burger",
         menu: "Lunch",
-        price: 14.39
+        price: 14.39,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Triple Dipper",
         menu: "Lunch",
         price: 17.59,
-        image: "assets/tD.avif"
+        Image: "../assets/tD.avif"
     },
     {
         name: "Bone-In Wings",
         menu: "Lunch",
-        price: 17.79
+        price: 17.79,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Boneless Wings",
         menu: "Lunch",
         price: 14.79,
+        Image: "../assets/tD.avif"
+    },
+    {
+        name: "Nashville Hot Chicken Crispers Combo",
+        menu: "Lunch",
+        price: 14.79,
+        Image: "../assets/tD.avif"
     },
     {
         name: "The Original Trio",
         menu: "Dinner",
         price: 26.49,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Full Rack Of Ribs",
         menu: "Dinner",
         price: 28.39,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Triple Dipper",
         menu: "Dinner",
         price: 17.59,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Bacon Cheeseburger",
         menu: "Dinner",
-        price: 15.59
+        price: 15.59,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Bacon Rancher Burger",
         menu: "Dinner",
-        price: 19.89
+        price: 19.89,
+        Image: "../assets/tD.avif"
     },
     {
         name: "Nashville Hot Big Crispy Chicken Sandwich",
         menu: "Dinner",
-        price: 15.99
-    }
+        price: 15.99,
+        Image: "../assets/tD.avif"
+    },
 ];
 
 const breakfast = document.getElementById("breakfast");
