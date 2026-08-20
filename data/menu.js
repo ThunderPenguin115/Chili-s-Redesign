ee52343767fc2e08961b418c928cd3a0f682f688
Matthew-Bueno <matthew.bueno09@gmail.com>
2026-08-20 07:51:46 -0700

    Added some imgs and changed the JS to now display names for each card

--- data/menu.js
+++ data/menu.js
@@ -4,124 +4,126 @@ const menuItems = [
         menu: "Breakfast",
         price: 15.99,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/waffles.jpg"
     },
     {
         name: "Pancakes",
         menu: "Breakfast",
         price: 13.59,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/pancakes.jpg"
     },
     {
         name: "Crepes",
         menu: "Breakfast",
         price: 14.99,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "French Toast",
         menu: 'Breakfast',
         price: 14.59,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Cinnamamon Buns",
         menu: "Breakfast",
         price: 17.99,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Briper",
         menu: "Breakfast",
         price: 25.59,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Honey-Chipotle Chicken Crispers Combo",
         menu: "Lunch",
         price: 17.69,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "The Big QP Burger",
         menu: "Lunch",
         price: 14.39,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Triple Dipper",
         menu: "Lunch",
         price: 17.59,
-        image: "assets/tD.avif",
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Bone-In Wings",
         menu: "Lunch",
         price: 17.79,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Boneless Wings",
         menu: "Lunch",
         price: 14.79,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Nashville Hot Chicken Crispers Combo",
         menu: "Lunch",
         price: 14.79,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
         },
     {
         name: "The Original Trio",
         menu: "Dinner",
         price: 26.49,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Full Rack Of Ribs",
         menu: "Dinner",
         price: 28.39,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Triple Dipper",
         menu: "Dinner",
         price: 17.59,
         quantity: 1,
-        Image: "../assets/tD.avif"
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Bacon Cheeseburger",
         menu: "Dinner",
         price: 15.59,
         quantity: 1,
-        Image: "../assets/tD.avif"},
+        Image: "../../assets/tD.avif"
+    },
     {
         name: "Bacon Rancher Burger",
         menu: "Dinner",
         price: 19.89,
-        quantity: 1
+        quantity: 1,
+        Image: "../../assets/tD.avif"
     },
     {
         name: "Nashville Hot Big Crispy Chicken Sandwich",
         menu: "Dinner",
         price: 15.99,
-        quantity: 1
+        quantity: 1,
+        Image: "../../assets/tD.avif"
     }
 ];
 
@@ -129,29 +131,35 @@ const breakfast = document.getElementById("breakfast");
 const lunch = document.getElementById("lunch");
 const dinner = document.getElementById("dinner");
 
-const cards = document.querySelectorAll(".card");
+const container = document.getElementById("container");
+
+function displayMenu(menu) {
+    container.innerHTML = "";
+    menuItems.filter(function(item){
+        return item.menu === menu;
+    }).forEach(function(item){
+        container.innerHTML += `
+        <div class="card">
+            <img src="${item.Image}" alt="${item.name}">
+            <h1>${item.name}</h1>
+            <p>${item.name} is one of our delicious ${item.menu.toLocaleLowerCase()} options!</p>
+            <div id="price">
+                <h3> Cost $${item.price}</h3>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+        `
+    })
+}
 
 breakfast.addEventListener("click", function(){
-    cards.forEach(function(card){
-        card.querySelectorAll("h1, p, img, #price").forEach(function(text){
-            text.style.visibility = "hidden";
-        });
-    });
-});
-
-dinner.addEventListener("click", function(){
-    cards.forEach(function(card){
-        card.querySelectorAll("h1, p, img, #price").forEach(function(text){
-            text.style.visibility = "hidden";
-        });
-    });
-});
+    displayMenu("Breakfast")
+})
 
 lunch.addEventListener("click", function(){
-    cards.forEach(function(card){
-        card.querySelectorAll("h1, p, img, #price").forEach(function(text){
-            text.style.visibility = "visible";
-        });
-    });
-});
+    displayMenu("Lunch")
+})
 
+dinner.addEventListener("click", function(){
+    displayMenu("Dinner")
+})
